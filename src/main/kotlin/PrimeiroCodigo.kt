9266144ba01ba171207c615f8c3a4bdcdd4012d7
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-08-28 13:40:38 -0300

    Curso de Kotlin | Básico #03 - Configurando o ambiente e criação do primeiro programa Kotlin

---
+++ src/main/kotlin/PrimeiroCodigo.kt
@@ -0,0 +1,10 @@
+/*Curso de Kotlin | Básico #03 - Configurando o ambiente e criação do primeiro programa Kotlin conforme:
+ *https://www.youtube.com/watch?v=A7mzcndP5lM&list=PLPs3nlHFeKTr-aDDvUxU971rPSVTyQ6Bn&index=3*/
+
+fun main(args: Array<String>){
+    //fun = palavra reservada para declarar uma função
+    //main = nome da função, no caso do main é o ponto de entrada
+    //args: Array<String> = tipo da variável é declarado depois do nome
+    println("Olá, Mundo!") //wrapper("embrulho") da biblioteca padrão do Java, com sintaxe mais concisa (System.out.println)
+    //é omitido o ; (ponto e vírgula)
+}
