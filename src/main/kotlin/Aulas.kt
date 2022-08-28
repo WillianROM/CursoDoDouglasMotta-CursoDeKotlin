f4de9ed3146832514ac9cbe015c71c5fc44c6dd1
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-08-28 19:38:50 -0300

    Curso de Kotlin | Básico #05 - Entendendo Variáveis (var|val)
    https://www.youtube.com/watch?v=cQC17DNfiQw&list=PLPs3nlHFeKTr-aDDvUxU971rPSVTyQ6Bn&index=6

--- src/main/kotlin/Aulas.kt
+++ src/main/kotlin/Aulas.kt
@@ -1,4 +1,4 @@
-/*
+
 /*Curso de Kotlin | Básico #03 - Configurando o ambiente e criação do primeiro programa Kotlin conforme:
  *https://www.youtube.com/watch?v=A7mzcndP5lM&list=PLPs3nlHFeKTr-aDDvUxU971rPSVTyQ6Bn&index=3*/
 
@@ -9,10 +9,10 @@ fun main(args: Array<String>){
     println("Olá, Mundo!") //wrapper("embrulho") da biblioteca padrão do Java, com sintaxe mais concisa (System.out.println)
     //é omitido o ; (ponto e vírgula)
 }
- */
+
 
 /* ************************************************************************************************************* */
-/*
+
 /* Curso de Kotlin | Básico #04 - Entendendo Funções
 * https://www.youtube.com/watch?v=yXHhVvJznuU&list=PLPs3nlHFeKTr-aDDvUxU971rPSVTyQ6Bn&index=5*/
 
@@ -32,7 +32,27 @@ fun maxBloco(a:Int,b:Int):Int{
 
     //Corpo de expressão é quando uma função consiste de apenas uma expressão (valor)
     fun maxExpressao(a:Int,b:Int):Int=if(a>b) a else b
-
     //No corpo de expressão podemos omitir o tipo de retorno
-   fun maxExoressaoSemTipoDeRetornoa:Int,b:Int)=if(a>b) a else b
-*/
+   //fun maxExoressaoSemTipoDeRetornoa:Int,b:Int)=if(a>b) a else b
+
+
+/* ************************************************************************************************************* */
+//https://www.youtube.com/watch?v=cQC17DNfiQw&list=PLPs3nlHFeKTr-aDDvUxU971rPSVTyQ6Bn&index=6
+//Curso de Kotlin | Básico #05 - Entendendo Variáveis (var|val)
+
+//Estrutura de uma variável
+/* Representa um valor ou expressão (na memórias)
+ * É declarada com a palavra-chave var|val seguido do nome
+ * O tipo da variável pode ser especificado ou não depois do nome
+ */
+
+val nome1 = "Douglas" //inferência de tipo
+var nome2:String = "João" //declaração do tipo
+/*var nome3:String
+nome3 = "Michel"*/
+
+//var vs val
+/* val (de valor) representa uma referência imutável, ou seja, seu estado não pode ser modificado após ser criado (jinal em Java)
+ * var (de variável) representa uma referência mutável, ou seja, o valor da variável pode ser alterado após ser criada (variável regular em Java)
+ * O tipo da variável pode ser especificado ou não depois do nome
+ *  */
---
+++ src/main/kotlin/Variaveis.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val nome:String = "Doug"
+    var idade:Int = 18
+    idade++
+
+    println("O $nome tem $idade anos de vida")
+}
